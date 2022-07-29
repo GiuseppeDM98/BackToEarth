c019ffc6e7f7d6553bb9099447a3c219a33662f6
Giuseppe Di Maio <gdimaio9814@gmail.com>
2022-07-29 17:24:36 +0200

    [Still following the course] Added new features
    Programmed the movement of the rocket:
    Now through the space bar the rocket goes up.
    Through A and D the rocket goes respectively to the left and to the right.
    
    Constraints have been added to the movement of the rocket:
    Now the rocket cannot rotate on the X and Y axis.
    Now the rocket cannot move on the Z axis
    
    Added method to manage gravity change when changing levels aka planet

--- BackToEarth/Assets/Scripts/Movement.cs
+++ BackToEarth/Assets/Scripts/Movement.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private Scene scene;
+    Rigidbody rb;
+    [SerializeField] private float mainThrustForce = 1000;
+    [SerializeField] private float rotationThrustForce = 100;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scene = SceneManager.GetActiveScene();
+        ChangeGravityBasedOnScene(scene.name);
+        rb = GetComponent<Rigidbody>();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -21,7 +29,8 @@ public class Movement : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("Pressed Space - Thrusting");
+            //scrivere Vector3.up è uguale a 0, 1, 0
+            rb.AddRelativeForce(mainThrustForce * Time.deltaTime * Vector3.up);
         }
     }
 
@@ -29,11 +38,31 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("Rotating Left");
+            //scrivere Vector3.forward è uguale a 0, 0, 1
+            ApplyRotation(rotationThrustForce);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("Rotating Right");
+            //scrivere Vector3.forward è uguale a 0, 0, -1
+            ApplyRotation(-rotationThrustForce);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(rotationThisFrame * Time.deltaTime * Vector3.forward);
+        rb.freezeRotation = false;
+    }
+
+    //Quando si cambierà livello idealmente cambierà anche la gravità
+    private void ChangeGravityBasedOnScene(string sceneName)
+    {
+        switch (sceneName)
+        {
+            case "Sandbox":
+                Physics.gravity = new Vector3(0, -9.81F, 0);
+                break;
         }
     }
 }
